86597748e4e315271d50bfa4c2ed039bc73a7fae
Debashis Gayen <debashisgayen02@gmail.com>
2023-01-15 19:39:27 +0530

    update lines

--- code/src/Arrays/Main.java
+++ code/src/Arrays/Main.java
@@ -3,11 +3,10 @@ public class Main {
     public static void main(String[] args) {
         var array = new implementArray(5);
         array.add(10);
-        array.add(10);
-        array.add(10);
-        array.add(10);
-        array.add(10);
-        array.add(10);
-        System.out.println(" ");
+        array.add(11);
+        array.add(12);
+        array.add(13);
+        array.add(14);
+        System.out.println(array.toString());
     }
 }
